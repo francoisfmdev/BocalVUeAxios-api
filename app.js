98218087f1b9e448c1281bece28807ea9453b37b
Francois MAESTRATI <francois.maestrati@gmail.com>
2020-09-27 17:54:16 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+/*
+Utiliser axios pour récuperer 
+le nom d'une ville quand un utilisateur saisi le code postal 
+*/
+
+var vm = new Vue({
+    el: "#app",
+    data: { postalCode= 0 },
+    watch: {
+        
+    }
+});
